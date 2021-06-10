547fbceb6b93111a7217e308a96b5db257158aa2
yatsun <yatsunlam25@gmail.com>
2021-06-10 00:38:44 -0400

    CRUD

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -2,6 +2,30 @@ class RecipeController < ApplicationController
 
     get "/recipes" do
         @recipes = Recipe.all
-        erb :recipes
+        erb :index
+    end
+
+    get "/recipes/:id" do
+
+    end
+
+    get "/recipes/new" do
+
+    end
+
+    post "/recipes" do
+
+    end
+
+    get "/recipes/:id/edit" do
+
+    end
+
+    patch "/recipes/:id" do
+
+    end
+
+    delete "/recipes/:id" do
+
     end
 end
