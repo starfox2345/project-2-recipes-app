1e558c8e4459d983d8f54f5999a4f40e2a04da5e
yatsun <yatsunlam25@gmail.com>
2021-06-11 00:57:51 -0400

    updated user migration

--- db/schema.rb
+++ db/schema.rb
@@ -28,9 +28,7 @@ ActiveRecord::Schema.define(version: 2021_06_09_021431) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "favorite_recipe"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
